e8389b4dd6675060913aac2897b5e243fbb9413e
Alex Lee <ahjlee712@gmail.com>
2021-02-21 20:54:44 -0500

    Routed the social links to proper footer elements

--- src/componentsList/FootersList.js
+++ src/componentsList/FootersList.js
@@ -1,7 +1,11 @@
 import Footer from '../components/Footer.js';
 
+const date = new Date().getFullYear;
+
 const FootersList = ({socials}) => (
   <div className = 'footer-list'>
+    <h1>Check out my socials below!</h1>
+    <p>Always in the mood to chat about codes, photography, or even just getting some coffee.</p>
     {socials.map(social => {
       return (
         <Footer
@@ -9,9 +13,10 @@ const FootersList = ({socials}) => (
           title = {social.title}
           link = {social.link}
           icon = {social.icon}
-      />
+        />
       )
     })}
+    <p class='copyright'>©{date} Designed and Built by Alex Lee</p>
   </div>
 );
 
